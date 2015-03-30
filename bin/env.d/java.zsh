6815dd9eace6201d63832ce31b965c643e77b2ee
Steven K. Sharp <steven.k.sharp@gmail.com>
2015-03-30 13:15:42 -0700

    Handles both Mac OS X and linux.

--- bin/env.d/java.zsh
+++ bin/env.d/java.zsh
@@ -1,10 +1,15 @@
-JDK6_HOME=`/usr/libexec/java_home --version 1.6 --failfast`
-JDK7_HOME=`/usr/libexec/java_home --version 1.7 --failfast`
-JDK8_HOME=`/usr/libexec/java_home --version 1.8 --failfast`
-JDK_HOME=${JDK8_HOME}
-if [[ ! ( ${PATH} =~ '.jdk' ) ]]; then
+if [[ -x /usr/libexec/java_home ]]; then
+    JDK6_HOME=$( /usr/libexec/java_home --version 1.6 --failfast )
+    JDK7_HOME=$( /usr/libexec/java_home --version 1.7 --failfast )
+    JDK8_HOME=$( /usr/libexec/java_home --version 1.8 --failfast )
+    JDK_HOME=${JDK8_HOME}
 	JAVA_HOME=${JDK_HOME}
+else
+    JDK_HOME=$( update-alternatives --query javac | grep '^Value' | sed -E 's#^.* (.+)/[^/]+/[^/]+$#\1#' )
+    JAVA_HOME=${JDK_HOME}
+fi
+if [[ ! ( ${PATH} =~ ${JAVA_HOME} ) ]]; then
 	PATH=${JAVA_HOME}/bin:${PATH}
 
-	export JAVA_HOME PATH
+	export JDK_HOME JAVA_HOME PATH
 fi
