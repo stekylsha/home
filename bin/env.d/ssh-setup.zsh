2c03e9058face0e5cc7ee1eb650644319641e880
Steven K. Sharp <steven.k.sharp@gmail.com>
2015-04-02 09:57:48 -0700

    Checks for ssh-agent before creating new instance.

--- bin/env.d/ssh-setup.zsh
+++ bin/env.d/ssh-setup.zsh
@@ -1,4 +1,14 @@
-eval "$(ssh-agent -s)"
-for f in ${HOME}/.ssh/id_rsa*; do
-    ssh-add ${f}
-done
+if [ -e ${HOME}/.SSH_AGENT ]; then
+    . ${HOME}/.SSH_AGENT
+fi    
+P_SSH_AGENT_PID="$( pgrep --uid ${USER} ssh-agent )"
+if [ -z "${P_SSH_AGENT_PID}" ] || [ "${P_SSH_AGENT_PID}" != "${SSH_AGENT_PID}" ]; then
+    rm -f ${HOME}/.SSH_AGENT
+    ssh-agent -s | grep -v 'Agent pid' > ${HOME}/.SSH_AGENT
+    . ${HOME}/.SSH_AGENT
+fi
+if [ -z "$( ssh-add -l )" ]; then
+    for f in ${HOME}/.ssh/id_rsa*; do
+        ssh-add ${f}
+    done
+fi
