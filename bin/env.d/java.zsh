184145a819c16ea37d9342b6b66e45c9cb639ff1
Steven K. Sharp <steven.k.sharp@gmail.com>
2015-04-02 09:57:08 -0700

    Mac OS X check.  Allows JDK_HOME override.

--- bin/env.d/java.zsh
+++ bin/env.d/java.zsh
@@ -1,15 +1,16 @@
-if [[ -x /usr/libexec/java_home ]]; then
-    JDK6_HOME=$( /usr/libexec/java_home --version 1.6 --failfast )
-    JDK7_HOME=$( /usr/libexec/java_home --version 1.7 --failfast )
-    JDK8_HOME=$( /usr/libexec/java_home --version 1.8 --failfast )
-    JDK_HOME=${JDK8_HOME}
-	JAVA_HOME=${JDK_HOME}
-else
-    JDK_HOME=$( update-alternatives --query javac | grep '^Value' | sed -E 's#^.* (.+)/[^/]+/[^/]+$#\1#' )
-    JAVA_HOME=${JDK_HOME}
+if [[ -z "${JDK_HOME}" ]] || [[ ! -e "${JDK_HOME}" ]]; then
+    if [[ -x /usr/libexec/java_home ]]; then
+        JDK6_HOME=$( /usr/libexec/java_home --version 1.6 --failfast )
+        JDK7_HOME=$( /usr/libexec/java_home --version 1.7 --failfast )
+        JDK8_HOME=$( /usr/libexec/java_home --version 1.8 --failfast )
+        JDK_HOME=${JDK8_HOME}
+    else
+        JDK_HOME=$( update-alternatives --query javac | grep '^Value' | sed -E 's#^.* (.+)/[^/]+/[^/]+$#\1#' )
+    fi
 fi
+JAVA_HOME=${JDK_HOME}
 if [[ ! ( ${PATH} =~ ${JAVA_HOME} ) ]]; then
-	PATH=${JAVA_HOME}/bin:${PATH}
+    PATH=${JAVA_HOME}/bin:${PATH}
 
-	export JDK_HOME JAVA_HOME PATH
+    export JDK_HOME JAVA_HOME PATH
 fi
