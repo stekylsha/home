008683408b45537b58307f31b0c8930217b6c931
stekylsha <steven.k.sharp@gmail.com>
2015-04-02 11:55:11 -0700

    Changed pgrep to generic -U option.  Changed ssh-add testing too.

--- bin/env.d/ssh-setup.zsh
+++ bin/env.d/ssh-setup.zsh
@@ -1,13 +1,14 @@
 if [ -e ${HOME}/.SSH_AGENT ]; then
     . ${HOME}/.SSH_AGENT
 fi    
-P_SSH_AGENT_PID="$( pgrep --uid ${USER} ssh-agent )"
+P_SSH_AGENT_PID="$( pgrep -U ${USER} ssh-agent )"
 if [ -z "${P_SSH_AGENT_PID}" ] || [ "${P_SSH_AGENT_PID}" != "${SSH_AGENT_PID}" ]; then
     rm -f ${HOME}/.SSH_AGENT
     ssh-agent -s | grep -v 'Agent pid' > ${HOME}/.SSH_AGENT
     . ${HOME}/.SSH_AGENT
 fi
-if [ -z "$( ssh-add -l )" ]; then
+SSH_IDENTITIES="$( ssh-add -l )"
+if [ $? -ne 0 ] || [ -z "${SSH_IDENTITIES}" ]; then
     for f in ${HOME}/.ssh/id_rsa*; do
         ssh-add ${f}
     done
