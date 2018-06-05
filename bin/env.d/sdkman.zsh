dd6554087b7a968cad12d675e5f2f2622d62a3b5
Steven K. Sharp <steven.k.sharp@gmail.com>
2018-06-04 18:34:53 -0700

    Corrected path for sdkman init script.

--- bin/env.d/sdkman.zsh
+++ bin/env.d/sdkman.zsh
@@ -1,5 +1,5 @@
 SDKMAN_DIR=${HOME}/.sdkman
 if [[ ! ( ${PATH} =~ 'sdkman' ) && -d "${SDKMAN_DIR}" ]]; then
 	export SDKMAN_DIR
-	[[ -s "/home/ss186187/.sdkman/bin/sdkman-init.sh" ]] && source "/home/ss186187/.sdkman/bin/sdkman-init.sh"
+	[[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
 fi
