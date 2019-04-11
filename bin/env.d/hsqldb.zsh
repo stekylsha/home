acbf49365722cdebebef5ecc3371fc44a14f7dfd
Steven K. Sharp <steven.k.sharp@gmail.com>
2019-04-11 12:33:58 -0700

    removed unused init scripts.

--- bin/env.d/hsqldb.zsh
+++
@@ -1,5 +0,0 @@
-HSQLDB_HOME=${HOME}/bin/hsqldb
-if [[ ! ( ${PATH} =~ 'hsqldb' ) && -d "${HSQLDB_HOME}" ]]; then
-	export PATH=${HSQLDB_HOME}/bin:${PATH}
-	export HSQLDB_HOME
-fi
--- bin/env.d/jruby.zsh
+++
@@ -1,7 +0,0 @@
-# No one uses jruby.  No one.
-#
-#JRUBY_HOME=${HOME}/bin/jruby
-#if [[ ! ( ${PATH} =~ 'jruby' ) && -d "${JRUBY_HOME}" ]]; then
-#	export PATH=${JRUBY_HOME}/bin:${PATH}
-#	export JRUBY_HOME
-#fi
